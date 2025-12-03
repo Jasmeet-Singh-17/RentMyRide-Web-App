6490f00e4bc65caa2cd1185f9ea7c554c823e5fe
jasmeeeetsingh17 <jasmeetsingh1710@gmail.com>
2025-12-03 17:00:24 +0530

    Fix GitHub Pages basePath config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
+  output: "export",                            // required for GitHub Pages
+  basePath: isProd ? "/RentMyRide-Web-App" : "",
+  assetPrefix: isProd ? "/RentMyRide-Web-App/" : "",
+
   images: {
     remotePatterns: [
       {
